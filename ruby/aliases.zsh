dc79f0ab8f9ba1cc999074997c1aee664ba1b8f6
Paz Aricha <paz@pazaricha.com>
2015-02-05 21:34:59 +0200

    some more usefull ruby aliases

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,3 +1,8 @@
+alias be='bundle exec'
+
+alias seed='be rake db:seed'
+alias migrate='be rake db:migrate && be rake db:test:prepare'
+
 alias sc='script/console'
 alias sg='script/generate'
 alias sd='script/destroy'
